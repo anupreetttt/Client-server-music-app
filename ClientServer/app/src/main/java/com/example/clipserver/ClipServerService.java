4431221264f223029012a7590d7c51a9911caf86
Anupreet Paulkar <apaulk2@uic.edu>
2022-12-02 04:10:37 -0600

    something

--- ClientServer/app/src/main/java/com/example/clipserver/ClipServerService.java
+++ ClientServer/app/src/main/java/com/example/clipserver/ClipServerService.java
@@ -20,9 +20,14 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ClipServerService extends Service {
 
-    private final static int[] audioArray = {R.raw.audio1, R.raw.audio2, R.raw.audio3, R.raw.audio4, R.raw.audio5, R.raw.audio6};
+    ArrayList <Integer> audioArray = new ArrayList<>(Arrays.asList(R.raw.audio1, R.raw.audio2, R.raw.audio3, R.raw.audio4, R.raw.audio5));
+//    private final static int[] audioArray = {R.raw.audio1, R.raw.audio2, R.raw.audio3, R.raw.audio4, R.raw.audio5, R.raw.audio6};
     public static final String CHANNEL_ID = "Audio service Channel";
     private MediaPlayer mediaPlayer;
 
@@ -32,10 +37,10 @@ public class ClipServerService extends Service {
         Log.i("ClipServerServices", "Service was created!") ;
 
         Intent intentNotification = new Intent(getApplicationContext(), ClipServerService.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intentNotification, PendingIntent.FLAG_IMMUTABLE);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intentNotification, PendingIntent.FLAG_IMMUTABLE);
         Notification notification = new Notification.Builder(getApplicationContext(), CHANNEL_ID).setContentTitle("Playing music")
                 .setSmallIcon(android.R.drawable.ic_media_play)
-                .setOngoing(true).setContentTitle("Playing music")
+                .setOngoing(true)
                 .setContentText("Music player")
                 .setTicker("Playing music")
                 .build();
@@ -48,21 +53,23 @@ public class ClipServerService extends Service {
 //        startForegroundService(i);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private void createNotificationChannel() {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel notificationChannel = new NotificationChannel(
                     CHANNEL_ID, "Audio player notification", NotificationManager.IMPORTANCE_LOW
             );
+            notificationChannel.setDescription("Player Notification");
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(notificationChannel);
         }
     }
 
-//    @Override
-//    public int onStartCommand(Intent intent, int flags, int startId) {
-//        super.onStartCommand(intent, flags, startId);
-//        return START_NOT_STICKY;
-//    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        return START_NOT_STICKY;
+    }
 
     @Nullable
     @Override
@@ -74,16 +81,20 @@ public class ClipServerService extends Service {
         super.onDestroy();
     }
 
+
     private final AudioInterface.Stub stubBinder = new AudioInterface.Stub() {
 
-        public void onStartService() throws RemoteException{}
+        public void onStartService() throws RemoteException {
+        }
 
-        public void playAudio(int audioChoice){
+        public void playAudio(int position) {
             if (mediaPlayer != null) {
                 mediaPlayer.stop();
                 mediaPlayer.release();
             }
-            mediaPlayer = MediaPlayer.create(getApplicationContext(), audioArray[audioChoice]);
+//            mediaPlayer = MediaPlayer.create(getApplicationContext(), audioArray[audioChoice]);
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), audioArray.get(position));
+
             mediaPlayer.start();
             if (mediaPlayer != null) {
                 mediaPlayer.setLooping(false);
@@ -98,33 +109,84 @@ public class ClipServerService extends Service {
                 });
             }
         }
+           //Attempt 1
+//        private final AudioInterface.Stub stubBinder = new AudioInterface.Stub() {
+//
+//            public void onStartService() throws RemoteException{}
+//
+//            public void playAudio(int audioChoice){
+//                if (mediaPlayer == null) {
+//                    mediaPlayer = MediaPlayer.create(getApplicationContext(), audioArray[audioChoice]);
+//                }
+//
+//                if (mediaPlayer != null) {
+//                    mediaPlayer.setLooping(false);
+//
+//                    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//                        @Override
+//                        public void onCompletion(MediaPlayer mp) {
+////                        mediaPlayer.stop();
+////                        mediaPlayer.release();
+////                        mediaPlayer = null;
+//                            onStop();
+//                        }
+//                    });
+//                }
+//                mediaPlayer.start();
+//            }
 
-        public void pauseAudio() throws RemoteException{
+        public void pauseAudio() throws RemoteException {
             mediaPlayer.pause();
         }
 
-        public void stopAudio() throws RemoteException{
+        public void stopAudio() throws RemoteException {
             mediaPlayer.stop();
             mediaPlayer.release();
             mediaPlayer = null;
 //                onStop();
         }
 
+//        public void pauseAudio() throws RemoteException{
+//            if (mediaPlayer != null) {
+//                mediaPlayer.pause();
+//            }
+//        }
+//
+//        public void stopAudio() throws RemoteException{
+//            if (mediaPlayer != null) {
+//                onStop();
+//            }
+//        }
+
+
         public void onResume() throws RemoteException {
             mediaPlayer.start();
-            if(mediaPlayer != null){
+            if (mediaPlayer != null) {
                 mediaPlayer.setLooping(false);
-                 mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                     @Override
-                     public void onCompletion(MediaPlayer mp) {
-                         mediaPlayer.stop();
-                         mediaPlayer.release();
-                         mediaPlayer = null;
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        mediaPlayer.stop();
+                        mediaPlayer.release();
+                        mediaPlayer = null;
 //                         stop();
-                     }
-                 });
+                    }
+                });
             }
         }
+
+//        public void onResume() throws RemoteException {
+//            mediaPlayer.start();
+//            if(mediaPlayer != null){
+//                mediaPlayer.setLooping(false);
+//                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//                    @Override
+//                    public void onCompletion(MediaPlayer mp) {
+//                        onStop();
+//                    }
+//                });
+//            }
+//        }
 //        public void onStop() {
 //            mediaPlayer.stop();
 //            mediaPlayer.release();
@@ -140,9 +202,8 @@ public class ClipServerService extends Service {
             stopSelf();
         }
 
-        public int trackStatus() throws RemoteException
-        {
-            if (mediaPlayer == null){
+        public int trackStatus() throws RemoteException {
+            if (mediaPlayer == null) {
                 return 0;
             } else if (mediaPlayer.isPlaying()) {
                 return 1;
@@ -151,5 +212,7 @@ public class ClipServerService extends Service {
             }
         }
     };
+
+
 
 }
